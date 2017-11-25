eab78d4642d743ab3ef1fedd3588932d605db319
Christopher King <cking@enmobilellc.com>
2017-11-25 15:14:02 -0500

    almost there - discovery service just needs broadcast out of docker

--- pyroku/roku.py
+++ pyroku/roku.py
@@ -1,38 +1,29 @@
 #!/usr/local/bin/python3
 
-import sys, socket, re, subprocess
-from openhab import openHAB
+import socket, re, requests
 
 
-def get_roku():
-    ssdpRequest = "M-SEARCH * HTTP/1.1\r\n" + \
-                  "HOST: 239.255.255.250:1900\r\n" + \
-                  "Man: \"ssdp:discover\"\r\n" + \
-                  "MX: 5\r\n" + \
-                  "ST: roku:ecp\r\n\r\n";
-    socket.setdefaulttimeout(10)
+def get_roku_ids():
+    ssdpRequest = "M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMan: \"ssdp:discover\"\r\nMX: 5\r\nST: roku:ecp\r\n\r\n"
+    socket.setdefaulttimeout(5)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
     sock.sendto(bytearray(ssdpRequest, 'utf-8'), ("239.255.255.250", 1900))
+    roku_list = {}
     while True:
         try:
             resp = str(sock.recv(1024), 'utf-8')
-            # print(resp)
-            # print("Matches")
-            matchObj = re.match(r'.*USN: uuid:roku:ecp:([\w\d]{12}).*LOCATION: (http://.*)/.*', resp, re.S)
-            return [matchObj.group(1), matchObj.group(2)]
+            matches = re.match(r'.*USN: uuid:roku:ecp:([\w\d]{12}).*LOCATION: (http://.*)/.*', resp, re.S)
+            roku_list[matches.group(1)] = matches.group(2)
         except socket.timeout:
             break
+    return roku_list
 
 
-def talk_roku(key):
-    base_url = get_roku()[1]
-    openhab = openHAB(base_url)
-
-    # fetch all items
-    items = openhab.fetch_all_items()
-    print(items)
+def roku_key(roku_id, key):
+    return requests.post('http://{}/keypress/{}'.format(roku_id, key), data={})
 
 
-talk_roku('Home')
+# roku_key('192.168.1.103:8060', 'Home')
+# print("{}".format(get_roku_ids()))
--- pyroku/server.py
+++ pyroku/server.py
@@ -1,13 +1,24 @@
 from flask import Flask, Response
 from os import environ
 from json import dumps as json_dumps
+from pyroku.roku import get_roku_ids, roku_key
 
 app = Flask(__name__)
 
 
+@app.route('/roku')
+def roku_list():
+    return Response(json_dumps(get_roku_ids()), mimetype='application/json')
+
+
+@app.route('/roku/<roku_id>/key/<key_id>')
+def key(roku_id, key_id):
+    return Response(json_dumps(roku_key(roku_id, key_id)), mimetype='application/json')
+
+
 @app.route('/')
 def root():
-    return Response(json_dumps({"test": "value"}), mimetype='application/json')
+    return Response(json_dumps({'root': 'root'}), mimetype='application/json')
 
 
 if __name__ == '__main__':
